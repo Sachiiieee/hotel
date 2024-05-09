abf490e5f18929e1df353915b4aa6b853426ef07
Monique Burilla <sachiecalee@gmail.com>
2024-05-09 16:24:21 +0800

    initial

---
+++ Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database{
+
+	const DB_HOST = "localhost";
+	const DB_USER = "root";
+	const DB_PWD = "";
+	const DB_NAME = "cozyy";
+
+
+	protected function db_connect(){
+		$dsn = 'mysql:host='.self::DB_HOST.';dbname='.self::DB_NAME;
+		$pdo = new PDO($dsn, self::DB_USER, self::DB_PWD);
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+			PDO::FETCH_ASSOC);
+		return $pdo;
+	}
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cozy Corner</title>
+</head>
+
+<body>
+    <nav>
+        <div>
+            <img class="nav-logo" src="images/cclogo.png" alt="logo">
+            <span>Cozy Corner</span>
+        </div>
+        <div class="nav-link">
+            <div onclick="location.href = ''">Home</div>
+            <div onclick="location.href = '#about'">About Us</div>
+            <div onclick="location.href = '#contact'">Contacts</div>
+            <div onclick="location.href = 'Form.php'">Reservation</div>
+        </div>
+    </nav>
+    <section id="home">
+        <div></div>
+    </section>
+    <section></section>
+    <section></section>
+</body>
+
+</html>
